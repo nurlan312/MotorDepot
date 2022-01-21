eb660372907db29486c045b2fa1b2e6783b4de59
Nurik <nurlanasj@gamil.com>
2022-01-21 22:41:54 +0300

    1 этап

---
+++ src/com/company/Avtobaza.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Avtobaza {
+    private int id;
+    private String name;
+    private String driver;
+    private String status;
+
+   public static Avtobaza makeAvtobaza(int id, String name, String driver, String status) {
+       try {
+           Avtobaza avtobaza = new Avtobaza();
+           avtobaza.id = id;
+           avtobaza.name = name;
+           avtobaza.driver = driver;
+           avtobaza.status = status;
+           return avtobaza;
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+       return null;
+   }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        try {
+            this.id = id;
+        } catch (RuntimeException e) {
+            e.getMessage();
+        }
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+       try {
+           this.name = name;
+       } catch (RuntimeException e) {
+           e.getMessage();
+       }
+    }
+    public String getDriver() {
+        return driver;
+    }
+    public void setDriver(String driver) {
+       try {
+           this.driver = driver;
+       } catch (RuntimeException e) {
+           e.getMessage();
+       }
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+       try {
+           this.status = status;
+       } catch (RuntimeException e) {
+           e.getMessage();
+       }
+    }
+
+    @Override
+    public String toString() {
+        return  id + "| " +
+                name + "| " +
+                driver + "| " +
+                status;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,64 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
+    private static final GsonBuilder BUILDER = new GsonBuilder();
+    private static final Gson GSON = BUILDER.setPrettyPrinting().create();
+    private static final Path WRITE_PATH = Paths.get("./avtobaza.gson");
 
     public static void main(String[] args) {
-	// write your code here
+        Avtobaza[] avtobazas = {
+                Avtobaza.makeAvtobaza(1, "Renault", "       ", "On Base"),
+                Avtobaza.makeAvtobaza(2, "Volvo", "       ", "On Base"),
+                Avtobaza.makeAvtobaza(3, "DAF XT", "       ", "On Base"),
+        };
+
+//        String avtobaza = GSON.toJson(avtobazas);
+//        write(avtobaza);
+//        System.out.println(avtobaza);
+//        String read = readFile();
+//        System.out.println(read);
+        System.out.println("1 этап: ");
+        System.out.println("# | Bus    | Driver | Status \n" +
+                "--+--------+--------+------- ");
+        Avtobaza[] avtobazas1 = GSON.fromJson(readFile(), Avtobaza[].class);
+        for (Avtobaza a : avtobazas) {
+            System.out.println(a);
+        }
     }
+
+    private static void write(String obj) {
+        Path write = Paths.get(String.valueOf(WRITE_PATH));
+        try {
+            Files.writeString(write, obj, StandardOpenOption.CREATE,
+                              StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+     private static String readFile() {
+        StringBuilder json = new StringBuilder();
+        try {
+            FileReader fr = new FileReader(String.valueOf(WRITE_PATH));
+            int a;
+            while ((a = fr.read()) != -1) {
+                json.append((char) a);
+            }
+            return json.toString();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return json.toString();
+     }
 }
